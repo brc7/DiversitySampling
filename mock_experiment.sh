f7dcc88b4bc66c4a72b3005dc1c0bc39a39dbf40
benitogeordie <bg31@rice.edu>
2020-04-01 16:28:41 +0700

    made bash scripts for experiments

--- mock_experiment.sh
+++ mock_experiment.sh
@@ -19,11 +19,11 @@
     # exit temp
     # rm temp
   # Exit directory
-timing_file = "/home/bg31/firstRaceProject/DiversitySampling/results/experiment_timing.txt"
-directorypath = "/home/bg31/firstRaceProject/DiversitySampling/part_"
-race = "/home/bg31/firstRaceProject/DiversitySampling/bin/sampleracesavable"
-taus = "1,1.7,2.8,4.5,7.7,12.9,21.5,35.9,59.9,100"
-outputs = "/home/bg31/firstRaceProject/DiversitySampling/results/experiment_sample_1 /home/bg31/firstRaceProject/DiversitySampling/results/experiment_sample_2"
+timing_file="/home/bg31/firstRaceProject/DiversitySampling/results/experiment_timing.txt"
+directorypath="/home/bg31/firstRaceProject/DiversitySampling/part_"
+race="/home/bg31/firstRaceProject/DiversitySampling/bin/sampleracesavable"
+taus="1,1.7,2.8,4.5,7.7,12.9,21.5,35.9,59.9,100"
+outputs="/home/bg31/firstRaceProject/DiversitySampling/results/experiment_sample_1 /home/bg31/firstRaceProject/DiversitySampling/results/experiment_sample_2"
 
 for dir in 0; do
   echo working on part ${dir}
@@ -36,7 +36,7 @@ for dir in 0; do
   cd temp
   for f1 in *_1_reads.fq; do
     f2=${f:0:10}_2_reads.fq
-    mytime = "$(time ${race} ${taus} PE experimentsavefile.bin ${f1} ${f2} ${outputs} --range 10 --k 15)"
+    mytime="$(time ${race} ${taus} PE experimentsavefile.bin ${f1} ${f2} ${outputs} --range 10 --k 15)"
     echo ${mytime}
     ${mytime} >> ${timing file}
   done
