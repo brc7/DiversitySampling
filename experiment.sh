211e92f5dfd0220ecada4fe7c1bcec398b79126f
benitogeordie <bg31@rice.edu>
2020-04-01 16:37:03 +0700

    made bash scripts for experiments

--- experiment.sh
+++ experiment.sh
@@ -31,7 +31,8 @@ for dir in 0 1 2 3 4 5 6 7 8 9; do
   mkdir temp
   for f in *.gz; do
     STEM=$(basename "${f}" .gz)
-    gunzip -c "${f}" > /temp/"${STEM}"
+    gunzip -c "${f}"
+    mv "${STEM}" > /temp/"${STEM}"
   done
   cd temp
   for f1 in *_1_reads.fq; do
