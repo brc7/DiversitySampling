44a0be679fe86e5b67d865b3c33b01282f8fbb59
benitogeordie <bg31@rice.edu>
2020-04-01 18:56:02 +0700

    shell

--- mock_experiment.sh
+++ mock_experiment.sh
@@ -1,6 +1,7 @@
 timing_file="/home/bg31/firstRaceProject/DiversitySampling/results/experiment_timing.txt"
 directorypath="/home/bg31/firstRaceProject/DiversitySampling/part_"
 race="/home/bg31/firstRaceProject/DiversitySampling/bin/sampleracesavable"
+savefile="/home/bg31/firstRaceProject/DiversitySampling/experimentsavefile.bin"
 taus="1,1.7,2.8,4.5,7.7,12.9,21.5,35.9,59.9,100"
 outputs="/home/bg31/firstRaceProject/DiversitySampling/results/experiment_sample_1 /home/bg31/firstRaceProject/DiversitySampling/results/experiment_sample_2"
 
@@ -16,7 +17,7 @@ for dir in 0; do
   gunzip *
   for f1 in *_1.fastq; do
     f2=${f1:0:13}_2.fastq
-    (time ${race} ${taus} PE experimentsavefile.bin ${f1} ${f2} ${outputs} --range 10 --k 15) >> ${timing_file}
+    (time ${race} ${taus} PE ${savefile} ${f1} ${f2} ${outputs} --range 10 --k 15) >> ${timing_file}
   done
   cd ..
   pwd
