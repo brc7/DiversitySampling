11c8e42aa63ba37cae6d37eac2a955c5d05a3f8c
benitogeordie <bg31@rice.edu>
2020-04-03 22:18:11 +0700

    shell

--- experiment.sh
+++ experiment.sh
@@ -32,8 +32,12 @@ cd /home/public_data/hmp2/chunxiao_download_2_27_2020_hmp2
 mkdir ${temp}
 find . -maxdepth 1 -name '*.gz' -exec cp {} ${temp}/{} \;
 cd ${temp}
-gunzip *
-for f1 in *_1_reads.fq; do
+for z1 in *_1_reads.fq.gz; do
+    echo ${z1}
+    gunzip z1
+    z2=${z1:0:10}_2_reads.fq.gz
+    gunzip z2
+    f1=${z1:0:10}_1_reads.fq
     echo ${f1}
     f2=${f1:0:10}_2_reads.fq
     mytime="$(time ( ${race} ${taus} PE ${savefile} ${f1} ${f2} ${outputs} --range 500000 --k 15 ) 2>&1 1>/dev/null )"
