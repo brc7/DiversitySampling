bef3a71ed9ef38405fb3b34e7cb4183d54d2034a
benitogeordie <bg31@rice.edu>
2020-04-04 19:56:23 +0700

    shell

--- multi_file_experiment.sh
+++ multi_file_experiment.sh
@@ -2,7 +2,7 @@ timing_file="./results/multifile_experiment_timing.txt"
 race="./bin/sampleracemanyfilestaus"
 temp="./unzipped"
 taus="1,1.7,2.8,4.5,7.7,12.9,21.5,35.9,59.9,100"
-outputs="./results/multifile_experiment_sample_1 ./multifile_experiment_sample_2"
+outputs="./results/multifile_experiment_sample_1 ./results/multifile_experiment_sample_2"
 inputlistfromrepo="./inputlist.txt"
 inputlistfromtemp="../inputlist.txt"
 
