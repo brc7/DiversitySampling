136dcfdc7fb9d151b2c2bebfbc556ac49cc0162a
benitogeordie <bg31@rice.edu>
2020-04-04 19:52:58 +0700

    shell

---
+++ multi_file_experiment.sh
@@ -0,0 +1,16 @@
+timing_file="./results/multifile_experiment_timing.txt"
+race="./bin/sampleracemanyfilestaus"
+temp="./unzipped"
+taus="1,1.7,2.8,4.5,7.7,12.9,21.5,35.9,59.9,100"
+outputs="./results/multifile_experiment_sample_1 ./multifile_experiment_sample_2"
+inputlistfromrepo="./inputlist.txt"
+inputlistfromtemp="../inputlist.txt"
+
+# Make file for file list
+
+cd ${temp}
+pwd > ${inputlistfromtemp}
+cd ..
+ls ${temp} > ${inputlistfromrepo}
+mytime="$(time ( ${race} ${taus} PE ${inputlistfromrepo} ${outputs} --range 500000 --k 15 ) 2>&1 1>/dev/null )"
+echo ${mytime} >> ${timing_file}
